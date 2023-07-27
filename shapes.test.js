3fe428582a096473ef2278d23d9467d5330971c8
Ismail <63372291+elixit@users.noreply.github.com>
2023-07-27 13:54:01 -0700

    Update shapes.test.js

--- shapes.test.js
+++ shapes.test.js
@@ -7,15 +7,15 @@ expect(shapes.colorToshape()).toEqual(`<rect x="20" width="90" height="90" fill=
 })
 
 const get = require('./shapes')
-let tShapes = Circle()
+let cShapes = Circle()
 test('check square color', () => {
-tShapes.addColor("blue") 
-expect(tShapes.colorToshape()).toEqual(`<circle cx="40" cy="50" r="55" fill=" fill="blue"/>`)
+cShapes.addColor("blue") 
+expect(cShapes.colorToshape()).toEqual(`<circle cx="40" cy="50" r="55" fill=" fill="blue"/>`)
 })
 
 const get = require('./shapes')
-let cShapes = Triangle()
+let tShapes = Triangle()
 test('check square color', () => {
-cShapes.addColor("blue") 
-expect(cShapes.colorToshape()).toEqual(`<polygon points="50,0 100,100 0,100" fill="blue"/>`)
+tShapes.addColor("blue") 
+expect(tShapes.colorToshape()).toEqual(`<polygon points="50,0 100,100 0,100" fill="blue"/>`)
 })
